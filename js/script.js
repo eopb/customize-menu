7089ffe8cc1596b97dd9c78b8c03b6e3942a0bde
Ethan Brierley <ethanboxx@gmail.com>
2018-10-02 14:57:22 +0100

    Useless js

--- js/script.js
+++ js/script.js
@@ -23,3 +23,7 @@ function smallFont() {
     var parag = document.getElementById("parag");
     parag.style.fontSize = "16px";
 }
+
+function addText() {
+    document.getElementById("parag").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum!";
+}
